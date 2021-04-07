ff2d78b0edf4629e900f4c678616f9eabebf123e
DerekBaert <derek.baert@gmail.com>
2021-04-07 00:06:25 -0500

    User replace profile photo
    Created an input for user to upload new photo, have not tested or added errors yet.

--- user_profile.php
+++ user_profile.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
     require 'header.php';
+    require 'ProjectFunctions.php';
+    use Gumlet\ImageResize;
+    require 'ImageResize.php';    
+    require 'ImageResizeException.php';
 
     $isUser = false;
 
@@ -8,7 +12,7 @@
 
     if(isset($_SESSION['user']))
     {
-        if($_SESSION['user']['id'] === $userId)
+        if(intval($_SESSION['user']['id']) === $userId)
         {
             $isUser = true;
         }        
@@ -23,13 +27,64 @@
     $reviewsQuery = "SELECT * FROM reviews WHERE user_id = " . $userId;
     $reviewsStatement = $db->prepare($reviewsQuery);
     $reviewsStatement->execute();
+
+    $validFile = false;
+
+    if($_POST)
+    {
+        $image_upload = isset($_FILES['profile']) && ($_FILES['profile']['error'] === 0);
+ 
+        if ($image_upload) 
+        {
+            $temp                 = explode(".", $_FILES["profile"]["name"]);     
+            $image_filename       = round(microtime(true)) . '.' . end($temp);;
+            $temporary_path       = $_FILES['profile']['tmp_name'];
+            $new_image_path       = file_upload_path($image_filename);            
+
+            if (file_is_an_image($temporary_path, $new_image_path)) 
+            {
+                move_uploaded_file($temporary_path, $new_image_path);                 
+
+                $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $image_filename);
+                $fileExtension   = pathinfo($image_filename, PATHINFO_EXTENSION);
+
+                $image = new ImageResize($new_image_path);
+                $image->resizeToWidth(400);
+                $image->save(file_upload_path($withoutExt . '_Medium' . '.' . $fileExtension));  
+
+                $image = new ImageResize($new_image_path);
+                $image->resizeToWidth(75);
+                $image->save(file_upload_path($withoutExt . '_Thumbnail' . '.' . $fileExtension));  
+                $imagePath = $withoutExt . '.' . $fileExtension;
+                $validFile = true;
+            }
+            else
+            {
+                $validFile = false;
+            }      
+
+            if($validFile)
+            {
+                $query = "UPDATE user_images SET original = :Original, thumbnail = :Thumbnail,  medium = :Medium values WHERE id = " . $_SESSION['user']['profile_picture'];
+                $statement = $db->prepare($query); 
+                $statement->bindValue(':Original', $imagePath);
+                $statement->bindValue(':Thumbnail', $withoutExt . '_Thumbnail' . '.' . $fileExtension);
+                $statement->bindValue(':Medium', $withoutExt . '_Medium' . '.' . $fileExtension);
+                $statement->execute();
+            }
+        }
+    }
+
 ?>
 
 <div class="container userInfo">
         <div class="userImage">
             <img src="profile_images/<?=$row[0]['medium']?>" alt="profile_image">
             <?php if($isUser) : ?>
-                <input type="file"
+            <form method='post' action="user_profile.php?id= <?=$userId?>">
+                <label for="image">Upload New Profile Picture:</label>
+                <input type="file" id="profile" name="profile">
+            </form>
             <?php endif ?>
         </div>
         <div class="userData">
